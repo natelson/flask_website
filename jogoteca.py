9967bf47b6af05811a9fd584af0a83f60a703538
Natelson <natelson.nascimento@gmail.com>
2022-05-24 14:43:22 -0300

    Creating login and session

--- jogoteca.py
+++ jogoteca.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 from game import Game
 app = Flask(__name__)
+app.secret_key = 'banana'
 
 game1 = Game('Super Mario', 'Adventure', 'NES')
 game2 = Game('HALO', 'Action', 'XBOX')
@@ -16,6 +17,22 @@ def index():
 def create_game():
     return render_template('newgame.html', title='New Game')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/authentication', methods=['POST',])
+def authentication():
+    if request.form:
+        if 'master' == request.form['password']:
+            session['logged_user'] = request.form['login']
+            flash('Welcome ' + request.form['login'])
+            return redirect('/')
+        else:
+            flash('Try again,  ' + request.form['login'])
+            return redirect('/login')
+
+
 @app.route('/create_game_post', methods=['POST',])
 def create_game_post():
     if request.form:
@@ -27,5 +44,11 @@ def create_game_post():
         games_list.append(game)
 
         return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session['logged_user'] = None
+    flash('Desconected')
+    return redirect('/login')
 
 app.run(debug=True)
